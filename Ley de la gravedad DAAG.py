e884824266225b01060a4efb8e48a36b17c8fec7
Alejo2030 <124615465+Alejo2030@users.noreply.github.com>
2023-03-22 22:44:34 -0500

    Add files via upload

---
+++ Ley de la gravedad DAAG.py
@@ -0,0 +1,15 @@
+G : float 
+G = 6.67384e-11
+
+def fuerza_gravedad_entre_dos_objetos(masa1, masa2, distancia):
+    fuerza = G * (masa1 * masa2) / distancia**2
+    return fuerza
+
+if __name__ == "__main__":
+    m1 = float(input("Ingrese la masa en Kilogramos del primer objeto: "))
+    m2 = float(input("Ingrese la masa en Kilogramos del segundo objeto: "))
+    d = float(input("Ingrese la distancia en metros que los separa: "))
+
+    fuerza_gravedad = fuerza_gravedad_entre_dos_objetos(m1, m2, d)
+
+    print("La fuerza de gravedad entre los dos objetos es:", fuerza_gravedad, "Newtons")
